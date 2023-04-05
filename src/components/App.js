785277d8695a874b6a3176bab740bd4c1943bb6c
sunit seth <100702483+sunitseth1@users.noreply.github.com>
2023-04-05 23:33:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-    <p data-ns-test="project-name">Dream Project</p>
-    <p data-ns-test="project-description">Project Description</p>
+    <p data-ns-test="project-name">I am learning React. </p>
+    <p data-ns-test="project-description"> My life is getting better.</p>
     </div>
   );
 }
